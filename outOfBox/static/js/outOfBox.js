5cb5fe0b084a5b2e8aa0fe1a0387a44012784586
timothejoubert <timothe.joubert@hotmail.fr>
2021-11-30 20:33:17 +0100

    design projets

--- outOfBox/static/js/outOfBox.js
+++ outOfBox/static/js/outOfBox.js
@@ -2,7 +2,7 @@
 
 var topScroll = window.pageYOffset || document.documentElement.scrollTop;
 
-const header = document.getElementById("accueil").getBoundingClientRect().height;
+const header = document.getElementById("wordscloud").getBoundingClientRect().height + document.getElementById("wordscloud").getBoundingClientRect().top;
 const nav = document.getElementsByTagName("nav")[0].getBoundingClientRect().height;
 
 const handleScroll = () => {
@@ -15,3 +15,25 @@ const handleScroll = () => {
 }
 
 window.addEventListener("scroll", handleScroll);
+
+
+const allImage = document.querySelectorAll('.container-project_img');
+let oddImg = [];
+let evenImg = [];
+allImage.forEach( (el, i) => {
+	if(i % 2 === 0){
+		addParallax(el, 'odd');
+	}else{
+		addParallax(el, 'even');
+	}
+})
+function addParallax(els, order){
+	new simpleParallax(els, {
+		delay: 0.5,
+		orientation: order === 'even' ? 'down right': 'up left',
+		scale: 1.3,
+		overflow: true,
+	});
+}
+
+
